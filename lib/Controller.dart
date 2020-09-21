494bdd213821e389f6ea1ce796a69ead8aa47a9d
Phan <36981556+phanngl@users.noreply.github.com>
2020-09-21 08:07:45 +0700

    initial commit

---
+++ lib/Controller.dart
@@ -0,0 +1,33 @@
+import "package:get/get.dart";
+import "package:vibration/vibration.dart";
+import 'package:quiver/async.dart';
+
+class Controller extends GetxController {
+  var count = 0.obs;
+  var isRunning = false.obs;
+  CountdownTimer cdTimer;
+
+  Controller() {
+    //init();
+  }
+
+  void init() {
+    count.value = 10;
+  }
+
+  void startTimer() {
+    init();
+
+    isRunning.value = true;
+    cdTimer = CountdownTimer(
+        Duration(seconds: this.count.value), Duration(seconds: 1));
+    cdTimer.listen((event) {})
+      ..onData((data) {
+        this.count.value--;
+      })
+      ..onDone(() {
+        isRunning.value = false;
+        Vibration.vibrate();
+      });
+  }
+}
